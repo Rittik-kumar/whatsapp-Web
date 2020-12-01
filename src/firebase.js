6201145290f2330d645de785e613d45fa66d86cd
Rittik Kumar <rkrittik17@gmail.com>
2020-12-01 19:19:46 +0530

    Setting up VSCode and Firebase

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAtSwVS5xINmAiqWtiC8JqyxOfrb6JT7w4",
+  authDomain: "whatsapp-clone-c1fe0.firebaseapp.com",
+  databaseURL: "https://whatsapp-clone-c1fe0.firebaseio.com",
+  projectId: "whatsapp-clone-c1fe0",
+  storageBucket: "whatsapp-clone-c1fe0.appspot.com",
+  messagingSenderId: "569736070219",
+  appId: "1:569736070219:web:c004318183f484542039ce"
+};
